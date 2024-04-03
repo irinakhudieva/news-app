1bdf995bf6a9c794889bb2ff879faa1667469c49
ikhudieva <iranapolova@mail.ru>
2024-04-03 14:02:44 +0300

    add news page and react router

--- src/pages/main/ui/LatestNews/LatestNews.tsx
+++ src/pages/main/ui/LatestNews/LatestNews.tsx
@@ -1,9 +1,22 @@
+import { useAppDispatch } from '../../../../app/appStrore'
+import { INews } from '../../../../entities/news'
 import { useGetLatestNewsQuery } from '../../../../entities/news/api/newsApi'
+import { setCurrentNews } from '../../../../entities/news/model/newsSlice'
 import { NewsList } from '../../../../widgets/news'
 import styles from './LatestNews.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const LatestNews = () => {
-  const {data, isLoading} = useGetLatestNewsQuery(null)
+    const {data, isLoading} = useGetLatestNewsQuery(null)
+
+    const dispatch = useAppDispatch()
+
+    const navigate = useNavigate()
+
+    const navigateTo = (news: INews) => {
+      dispatch(setCurrentNews(news))
+      navigate(`/news/${news.id}`)
+    }
 
     return (
       <section className={styles.section}>
@@ -12,6 +25,8 @@ const LatestNews = () => {
             type='banner' 
             news={data && data.news} 
             isLoading={isLoading} 
+            viewNewsSlot={(news: INews) => 
+              <p onClick={() => navigateTo(news)} className={styles.readMore}>view more...</p>}
           />
       </section>
     )
